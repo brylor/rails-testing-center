2e7a587626430c26baba569cf74df8fad0c368a1
Bryan Taylor <btaylor@alpharender.net>
2012-08-09 20:47:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillsassessment::Application.config.secret_token = 'd3a7195f4397d9e5e09de97352f839a2c34dbc925e0e714763bc7c992170279608f6a98952692f91f4a13d0c64950f6f3a993b3a03578e211df1a025365a5b51'
